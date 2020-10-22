de6260b6c138cbd2729f39a9d24a457283732b01
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-10-22 18:46:23 +0200

    Update Arduino_NetworkConnection_Reboot-router.ino

--- Arduino_NetworkConnection_Reboot-router.ino
+++ Arduino_NetworkConnection_Reboot-router.ino
@@ -4,21 +4,21 @@
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte server[] = { "http://www.google.se" }; // Google
-int failedConnect = 0;
-int failedConnectGlobal = 0;
+int failedConnect = 0;  //Connection failed count, 3 times reboot router
+int failedConnectGlobal = 0;  //Reboot counter
 EthernetClient client;
-const int ledPin =  2;// the number of the LED pin
+const int relayPin =  2;  //Relay pin
 
 void setup()
 {
   Ethernet.begin(mac);
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(ledPin, LOW);
+  digitalWrite(relayPin, LOW);
   delay(60000);
   Serial.print("...");
   if (client.connect(server, 80)) {
@@ -28,7 +28,7 @@ void loop()
     Serial.print("~");
     failedConnect = failedConnect + 1;
     if(failedConnect>=3){
-      digitalWrite(ledPin, HIGH);
+      digitalWrite(relayPin, HIGH);
       delay(5000);
       failedConnectGlobal = failedConnectGlobal + 1;
       failedConnect = 0;
